fe4fda4f86b2f4ef138bdffee83e69ffe7535a7d
SirArsen1 <arsen.aloyan.26@gmail.com>
2025-04-12 23:09:49 +0200

    Balanced the speed, added ifelse statements to tune the speed of typing

--- assignments/week1/ASCII assignment v2.py
+++ assignments/week1/ASCII assignment v2.py
@@ -3,21 +3,34 @@ import time,sys
 art = ('''
           /\____/\ 
         _(@ ).(@ )|_
-         \   ^    /_______ 
+         \   ^    /_______
           |               \ 
           |  n  r-----|    \ 
-          |  || |   | |  |\ \ 
-          
-          My attempt at creating a cat, 
-          without using online ascii-art generators
-          ''')
+          |  || |   | |  |\ \ ''')
+
+explainer = ('''\nMy attempt at creating a cat,\nwithout using online ascii-art generators''')
 
 def typew(art):
     for char in art:
         sys.stdout.write(char)
         sys.stdout.flush()
+        time.sleep(0.05)
+        if char !=(" ","\n"):
+            time.sleep(0.0001)
+        else:
+            time.sleep(0.05)
+
+def typew2(explainer):
+    for char in explainer:
+        sys.stdout.write(char)
+        sys.stdout.flush()
         time.sleep(0.1)
+        if char !="\n":
+            time.sleep(0.0001)
+        else:
+            time.sleep(0.1)
 
 typew(art)
+typew2(explainer)
 
 #tutorial used: https://youtu.be/2h8e0tXHfk0?si=LQCgwk4QisDNc50y
